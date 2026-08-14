6243c4e4e477fef134a65ce30ccb85f38a68f59d
Yadlapalli Sriharsha <yadlapalliharsha57@gmail.com>
2026-08-14 19:49:13 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            int diff=nums[i]-nums[i-1];
+            if(diff==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
